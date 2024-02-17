982aa393e19d753fcfe1208106fb3c22705d8fb0
mrkiyn <amado.markian@gmail.com>
2024-02-17 10:17:15 +0800

    Fixing something

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
   # Defines the root path route ("/")
   # root "posts#index"
-
+  #
   get '/products', to: 'products#index', as: :products
 
   get '/products/new', to: 'products#new' , as: :new_product
